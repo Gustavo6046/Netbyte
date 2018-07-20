f0b5b135809da0ff528ef5a821a1b39a9772cc81
Gustavo R. 'Gustavo6046' Rehermann <gugurehermann@gmail.com>
2018-07-20 19:19:58 -0300

    v0.1.4 (the previous two commit should have been v0.1.4, but I messed up :S )

--- netbyte.py
+++ netbyte.py
@@ -664,7 +664,7 @@ class VersionCheckError(BaseException):
         return self.msg
         
 class Netbyte(object):
-    VERSION = "0.1.3"
+    VERSION = "0.1.4"
 
     def __init__(self, print_stream=sys.stdout):
         self.variables = {
